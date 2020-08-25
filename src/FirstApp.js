53e183c05c215d6858cbb75719701902f8e3a7ca
Ismael López <ismaellopez.dev@gmail.com>
2020-08-25 16:25:32 -0500

    :alembic: firts steps
    Return elements in Fragment Component

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,8 +1,16 @@
 import React from 'react'
+// import React, { Fragment } from 'react'
 
 // Functional Components
 const FirstApp = () => {
-  return <h1>Hello World</h1>;
+  
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>My first application</p>
+    </>
+  );
+
 }
 
 export default FirstApp;

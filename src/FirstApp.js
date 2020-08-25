152ab427c6f141304a7bfc8d70f7b8348ac01a4e
Ismael López <ismaellopez.dev@gmail.com>
2020-08-25 17:48:26 -0500

    :alembic: first steps
    Comunication between components, making use of Props

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -2,19 +2,11 @@ import React from 'react'
 // import React, { Fragment } from 'react'
 
 // Functional Components
-const FirstApp = () => {
+const FirstApp = ({ greeting }) => {  
 
-  const greeting = 'Hello world message';
-
-  const object = {
-    name: 'Ismael',
-    age: 27
-  }; 
-  
   return (
     <>
-      <h1>{ greeting }</h1>
-      <pre>{ JSON.stringify(object, null, 3) }</pre>
+      <h1>{ greeting }</h1>      
       <p>My first application</p>
     </>
   );

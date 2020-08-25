0c3e43e6f597147e638788c6aafeb46842299662
Ismael López <ismaellopez.dev@gmail.com>
2020-08-25 16:37:30 -0500

    :alembic: first steps
    print variables in HTML

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -3,10 +3,18 @@ import React from 'react'
 
 // Functional Components
 const FirstApp = () => {
+
+  const greeting = 'Hello world message';
+
+  const object = {
+    name: 'Ismael',
+    age: 27
+  }; 
   
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>{ greeting }</h1>
+      <pre>{ JSON.stringify(object, null, 3) }</pre>
       <p>My first application</p>
     </>
   );

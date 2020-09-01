6840c6552fe2c2f59dedcd69d0610e2ce0740306
Ismael López <ismaellopez.dev@gmail.com>
2020-09-01 17:57:01 -0500

    :alembic: first steps
    CounterApp component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-import FirstApp from './FirstApp';
+import CounterApp from './CounterApp';
 import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( <FirstApp greeting="Hello world" /> , divRoot);
+ReactDOM.render( <CounterApp value={ 4 } /> , divRoot);

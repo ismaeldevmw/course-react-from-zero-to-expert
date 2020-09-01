2e81aa9156711dfe5187ae834b86a2f6aae5cc62
Ismael López <ismaellopez.dev@gmail.com>
2020-09-01 18:11:42 -0500

    :alembic: first steps
    onClick event

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
 
+  //handleAdd
+  const handleAdd = (e) => {
+    console.log(e);
+  }
+
   return(
     <>
       <h1>Counter App</h1>
       <h2>{ value }</h2>
+
+      <button onClick={ handleAdd }>+1</button>
     </>
   );
 

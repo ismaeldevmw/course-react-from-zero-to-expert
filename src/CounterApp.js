99a00bf8670fa6b9b5829677a4565f565bc4f897
Ismael López <ismaellopez.dev@gmail.com>
2020-09-03 11:04:32 -0500

    :alembic: first steps
    events handleSubtract and handleReset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
 
-  const [ counter, setCounter ] = useState(0);
+  const [ counter, setCounter ] = useState(value);
   
   
 
@@ -13,11 +13,16 @@ const CounterApp = ({ value }) => {
     setCounter( (c) => c + 1 );
   }
 
+  const handleSubtract = () => setCounter( counter - 1 );
+  const handleReset = () => setCounter( value );
+
   return(
     <>
       <h1>Counter App</h1>
       <h2>{ counter }</h2>
 
+      <button onClick={ handleSubtract }>-1</button>
+      <button onClick={ handleReset }>Reset</button>
       <button onClick={ handleAdd }>+1</button>
     </>
   );

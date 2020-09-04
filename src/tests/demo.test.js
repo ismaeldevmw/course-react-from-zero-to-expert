4625aba338d6b4553f87da781fce5444bca58802
Ismael López <ismaellopez.dev@gmail.com>
2020-09-04 17:44:34 -0500

    :white_check_mark: unit & integration tests
    Jets - expect & toBe functions

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,10 +1,17 @@
 
-test('It will be true', () => {
+describe('Tests into demo.test.js', () => {
+  
+  test('The strings must be equals ', () => {
+  
+    // 1. Initialization
+    const message = "Hello world!";
+    
+    // 2. Encouragement
+    const message2 = 'Hello world!';
+  
+    // 3. Watch the behavior
+    expect(message).toBe(message2);
+  
+  });
 
-  const isActive = false;
-
-  if( !isActive ) {
-    throw new Error('No is active');
-  }
-
-})
+});

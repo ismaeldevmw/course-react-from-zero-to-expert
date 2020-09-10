1eee0e176e0d92001dccb009bb6727795dd03756
Ismael López <ismaellopez.dev@gmail.com>
2020-09-10 15:55:16 -0500

    :construction: created AddCategory component

--- src/components/GifExpertApp.jsx
+++ src/components/GifExpertApp.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react'
+import { AddCategory } from './AddCategory';
 
 const GifExpertApp = () => {
 
     // const categories = ['One Punch', 'Samurai', 'Dragón'];
     const [categories, setCategories] = useState(['One Punch', 'Samurai', 'Dragón']);
 
-    const handleAdd = () => {
-        // setCategories([...categories, 'HunterXHunter']);
-        setCategories( cats => [...cats, 'HunterXHunter']);
-    }
+    // const handleAdd = () => {
+    //     // setCategories([...categories, 'HunterXHunter']);
+    //     setCategories( cats => [...cats, 'HunterXHunter']);
+    // }
 
     return (
         <>
 
             <h2>GifExpertApp</h2>
+            <AddCategory />
             <hr/>
-
-            <button onClick={ handleAdd }>Add</button>
 
             <ol>
                 {

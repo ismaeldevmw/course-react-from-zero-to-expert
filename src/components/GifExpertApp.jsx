a80aa0a3fdf18786ef5fc05ea65e89ff1b7e8a47
Ismael López <ismaellopez.dev@gmail.com>
2020-09-10 15:38:07 -0500

    :construction: list categories created

--- src/components/GifExpertApp.jsx
+++ src/components/GifExpertApp.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai', 'Dragón'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai', 'Dragón']);
+
+    const handleAdd = () => {
+        // setCategories([...categories, 'HunterXHunter']);
+        setCategories( cats => [...cats, 'HunterXHunter']);
+    }
+
     return (
         <>
+
             <h2>GifExpertApp</h2>
             <hr/>
+
+            <button onClick={ handleAdd }>Add</button>
+
+            <ol>
+                {
+                    categories.map( (category,  i) => {
+                        return <li key={ i }>{ category }</li>
+                    })
+                }
+            </ol>
         </>
     )
 }

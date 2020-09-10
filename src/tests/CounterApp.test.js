8be611342e57872f4525d422d51d5d79e150f3d6
Ismael López <ismaellopez.dev@gmail.com>
2020-09-10 11:25:51 -0500

    :white_check_mark: unit & integration tests
    simulate click events

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -6,9 +6,14 @@ import CounterApp from '../CounterApp';
 
 describe('Tests into <CounterApp />', () => {
     
+    let wrapper = shallow(<CounterApp />);
+    
+    beforeEach( () => {
+        wrapper = shallow(<CounterApp />);
+    })
+
     test('should show <CounterApp /> alright', () => {
         
-        const wrapper = shallow(<CounterApp />);
         expect(wrapper).toMatchSnapshot();
 
     });
@@ -19,6 +24,23 @@ describe('Tests into <CounterApp />', () => {
         const counterText = wrapper.find('h2').text().trim();
 
         expect( counterText ).toBe('100');
+    });
+
+    test('should increment with the button +1', () => {
+        
+        wrapper.find('button').at(2).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        expect( counterText ).toBe('11');
+
+    });
+
+    test('should dismiss with the button -1', () => {
+        
+        wrapper.find('button').first().simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        console.log(counterText);
+        expect( counterText ).toBe('9');
+
     });
 
 });

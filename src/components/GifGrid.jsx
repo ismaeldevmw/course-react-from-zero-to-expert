0113f795788b85e159c397848301cec6cdebefc8
Ismael López <ismaellopez.dev@gmail.com>
2020-09-11 12:35:01 -0500

    :construction: show image with GifGridItem component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [count, setCount] = useState(0);
+    const [images, setImages] = useState([]);
     
     // It allow us to run once when the page is rendered for the first time
     useEffect( () => {
@@ -23,13 +24,20 @@ export const GifGrid = ({ category }) => {
         })
 
         console.log(gifs);
+        setImages(gifs);
     }
     
     return (
         <div>
             <h3> { category } </h3>
-            <h3> { count } </h3>
-            <button onClick={() => setCount( count + 1) }>+</button>
+            {
+                images.map( img => (
+                    <GifGridItem 
+                        key={ img.id }
+                        { ...img }
+                    />
+                ))
+            }          
         </div>
     )
 }

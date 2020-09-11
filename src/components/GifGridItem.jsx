f65139c893414123dc81d5b9495c783b3d13f630
Ismael López <ismaellopez.dev@gmail.com>
2020-09-11 12:55:32 -0500

    :construction: adding styles with className property

--- src/components/GifGridItem.jsx
+++ src/components/GifGridItem.jsx
@@ -5,9 +5,11 @@ export const GifGridItem = ({ id, title, url }) => {
     console.log(id, title, url);
 
     return (
-        <div>
-            <img src={ url } alt={ title} />
-            <p>{ title }</p>
+        <div className="card">
+            <div className="card__body">
+                <img src={ url } alt={ title} />
+                <p>{ title }</p>
+            </div>
         </div>
     )
 }

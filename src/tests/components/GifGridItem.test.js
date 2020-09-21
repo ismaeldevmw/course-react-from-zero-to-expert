ec94f99e8483282902d59807d0e076a58c3d1b43
Ismael López <ismaellopez.dev@gmail.com>
2020-09-21 13:11:14 -0500

    :white_check_mark: tests in GifGridItem component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -8,11 +8,35 @@ describe('Tests into <GifGridItem />', () => {
 
     const title = 'The title';
     const url = 'https://localhost/image.jpg';
+    const wrapper = shallow(<GifGridItem  title={ title } url={ url } />);
     
     test('should show <GifGridItem /> alright', () => {
         
-        const wrapper = shallow(<GifGridItem  title={ title } url={ url } />);
         expect(wrapper).toMatchSnapshot();
+
+    });
+
+    test('should have a paragraph with the title', () => {
+        
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe( title );
+
+    });
+
+    test('should have an image equals to url and alt props', () => {
+        
+        const img = wrapper.find( 'img' );
+        expect( img.prop('src') ).toBe( url );
+        expect( img.prop('alt') ).toBe( title );
+
+    });
+
+    test('should have a class animate__fadeIn', () => {
+        
+        const div = wrapper.find('div.card');
+        const className = div.prop('className');
+
+        expect( className.includes('animate__fadeIn') ).toBe( true );
 
     });
 

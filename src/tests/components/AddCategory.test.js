9bd3385b7a4885718bdc645758569b595b4e0d5c
Ismael López <ismaellopez.dev@gmail.com>
2020-09-22 11:18:18 -0500

    :white_check_mark: simulate submit event

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,8 +4,13 @@ const { AddCategory } = require("../../components/AddCategory");
 
 describe('Tests into AddCategory component', () => {
     
-    const setCategories = () => {};
-    const wrapper =  shallow( <AddCategory setCategories={ setCategories } /> );
+    const setCategories = jest.fn();
+    let wrapper =  shallow( <AddCategory setCategories={ setCategories } /> );
+    
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper =  shallow( <AddCategory setCategories={ setCategories } /> );
+    });
 
     test('should show right now', () => {
         
@@ -18,6 +23,13 @@ describe('Tests into AddCategory component', () => {
         const input = wrapper.find('input');
         const textValue = 'One punch';
         input.simulate('change', { target: { textValue } });
+
+    });
+
+    test('shouldn`t post information when submit', () => {
+        
+        wrapper.find('form').simulate('submit', { preventDefault(){} });
+        expect( setCategories ).not.toHaveBeenCalled();
 
     });
 

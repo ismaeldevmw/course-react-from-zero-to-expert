c01116a38703ca27235aef49b40c43c716419904
Ismael López <ismaellopez.dev@gmail.com>
2020-09-22 10:09:52 -0500

    :white_check_mark: simulate input changes
    tests into AddCategory component

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -5,11 +5,19 @@ const { AddCategory } = require("../../components/AddCategory");
 describe('Tests into AddCategory component', () => {
     
     const setCategories = () => {};
+    const wrapper =  shallow( <AddCategory setCategories={ setCategories } /> );
 
     test('should show right now', () => {
         
-        const wrapper =  shallow( <AddCategory setCategories={ setCategories } /> );
         expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('should changes input text', () => {
+        
+        const input = wrapper.find('input');
+        const textValue = 'One punch';
+        input.simulate('change', { target: { textValue } });
 
     });
 

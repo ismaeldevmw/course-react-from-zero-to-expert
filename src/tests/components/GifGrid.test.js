50de11691520b7acbde1c86d7459e8c636c8a24f
Ismael López <ismaellopez.dev@gmail.com>
2020-09-28 12:03:08 -0500

    :white_check_mark: test Mock customHook into GifGrid

---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GifGrid } from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
+
+describe('Tests into GifGrid component', () => {
+    
+    
+    test('should show the component alright', () => {
+        
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true
+        });
+        
+        const wrapper = shallow(<GifGrid category="One Punch" />)
+        expect(wrapper).toMatchSnapshot();
+        
+    });
+    
+    test('should show items when load imagen with useFetchGifs', () => {
+
+        const gifs = [{
+            id: 'ABC',
+            url: 'https://localhosy/anything/thing.jpg',
+            title: 'Anything'
+        }]
+
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false
+        });
+        
+        const wrapper = shallow(<GifGrid category="One Punch" />)
+        expect(wrapper).toMatchSnapshot();
+
+    });
+});

c7a80f0916ba22151673d0b31cc2f80d66ede509
Ismael López <ismaellopez.dev@gmail.com>
2020-09-28 15:30:31 -0500

    :tada: began project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { HooksApp } from './HooksApp';
 
-const greeting = <h1>Hello World</h1>;
-const divRoot = document.querySelector('#root');
-
-ReactDOM.render( greeting, divRoot);
+ReactDOM.render( 
+    <HooksApp />, 
+    document.querySelector('#root')
+);

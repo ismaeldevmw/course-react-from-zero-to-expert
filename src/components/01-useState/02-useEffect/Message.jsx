9a1509d10d76bae59937c50dd7794339e85d413c
Ismael López <ismaellopez.dev@gmail.com>
2020-11-23 11:40:18 -0600

    :alembic: useEffect - Cautions to take

--- src/components/01-useState/02-useEffect/Message.jsx
+++ src/components/01-useState/02-useEffect/Message.jsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+    const [coordinates, setCoordinates] = useState({ x:0, y:0})
+    const { x, y } = coordinates
+
     useEffect(() => {
-        console.log('component mounted');
+
+        const mouseMove = (e) => {
+            const coordinates = { x: e.x, y: e.y }
+            setCoordinates(coordinates);
+        }
+
+        window.addEventListener('mousemove', mouseMove);
+
         return () => {
-            console.log('component unmounted');
+            window.removeEventListener('mousemove', mouseMove)
         };
     }, [])
 
     return (
         <div>
             <h3>Hello from Message</h3>
+            <p>
+                x:{x} y:{y}
+            </p>
         </div>
     )
 }

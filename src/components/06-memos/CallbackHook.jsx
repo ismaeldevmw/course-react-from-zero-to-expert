7eb4bc7644a76f70f5a5636deb1ce64201f42766
Ismael López <ismaellopez.dev@gmail.com>
2020-11-24 10:35:43 -0600

    :alembic: useCallback - use case
    + an implementation of useCallback Hook

---
+++ src/components/06-memos/CallbackHook.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { ShowIncrement } from './ShowIncrement';
+
+import './memorize.css';
+import { useCallback } from 'react';
+import { useEffect } from 'react';
+
+export const CallbackHook = () => {
+
+    const [counter, setCounter] = useState( 10 );
+
+    // const increment = () => {
+    //     setCounter( counter + 1);
+    // }
+
+    const increment = useCallback( (num) => {
+        setCounter( c => c + num);
+    }, [ setCounter ]);
+
+    useEffect( () => {
+        // ???
+    }, [increment]);
+
+    return (
+        <div>
+            <h1>useCallback Hook: { counter }</h1>
+            <hr/>
+
+            <ShowIncrement increment={ increment }/>
+
+        </div>
+    )
+}

705418c2e668550dc775fb0c956a7411a18ac3fc
Ismael López <ismaellopez.dev@gmail.com>
2020-12-13 18:38:43 -0600

    :white_check_mark: execute functions with act
    +testing increment, decrease & reset  functions

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import { useCounter } from "../../hooks/useCounter";
 
 describe('Tests in useCounter', () => {
@@ -12,5 +12,49 @@ describe('Tests in useCounter', () => {
         expect( typeof result.current.decrease ).toBe('function');
         expect( typeof result.current.reset).toBe('function');
     });
-    
+
+    test('should  increment counter by 1', () => {
+        
+        const { result } = renderHook( () => useCounter(100) );
+        const { increment } = result.current;
+
+        act( () => {
+            increment();
+        })
+
+        const { counter } = result.current;
+        expect( counter ).toBe(101)
+
+    });
+
+    test('should  decrease counter by 1', () => {
+        
+        const { result } = renderHook( () => useCounter(100) );
+        const { decrease } = result.current;
+
+        act( () => {
+            decrease();
+        })
+
+        const { counter } = result.current;
+        expect( counter ).toBe(99)
+
+    });
+
+
+    test('should  reset the initial state', () => {
+        
+        const { result } = renderHook( () => useCounter(100) );
+        const { increment, reset } = result.current;
+
+        act( () => {
+            increment();
+            reset();
+        })
+
+        const { counter } = result.current;
+        expect( counter ).toBe(100)
+
+    });
+
 });

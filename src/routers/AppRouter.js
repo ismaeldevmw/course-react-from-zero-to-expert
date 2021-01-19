271afe0770096ff6a9c931d4e04548976b259e8d
Ismael López <ismaellopez.dev@gmail.com>
2021-01-18 20:07:22 -0600

    :alembic: hold the authentication state

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,12 +4,31 @@ import {
     Switch,
     Route,
     Redirect
-  } from 'react-router-dom';
+} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { firebase } from '../firebase/firebase-config';
 
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        
+        firebase.auth().onAuthStateChanged( user => {
+        
+            if ( user?.uid ) {
+                dispatch( login( user.uid, user.displayName ) );
+            }
+            
+        });
+        
+    }, [ dispatch ]);
+    
     return (
         <Router>
             <div>
